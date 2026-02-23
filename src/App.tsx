3f33eee6d7ff77397c2e4436d0b163271c50f583
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2026-02-23 09:33:48 +0100

    set up tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import "./style/index.css";
+
  const App = () => {
   return (
-    <div>Hello world
+<div className="min-h-screen bg-yellow-300 flex items-center justify-center">
+      <h1 className="text-4xl font-black text-black">
+        Tailwind OK
+      </h1>
     </div>
   )
  }
