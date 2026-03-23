9fd8f17568067b24aecc042bbb055c46cdcc5a15
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2026-03-23 21:42:14 +0100

    .

--- src/pages/home/Hero.tsx
+++ src/pages/home/Hero.tsx
@@ -1,5 +1,5 @@
 import Section from '@/components/common/Section';
-import heloLulu from '@/pages/home/file_1.png';
+import heloLulu from '@/pages/home/file_000000005a987246a247f74797bca5e8 1 (1).webp';
 
 const Hero = () => {
   return (
