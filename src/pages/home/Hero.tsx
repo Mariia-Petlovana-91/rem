92ab5b240816aab30406f0d06e9b53f2f84be67e
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2026-03-23 21:46:27 +0100

    .

--- src/pages/home/Hero.tsx
+++ src/pages/home/Hero.tsx
@@ -1,11 +1,11 @@
 import Section from '@/components/common/Section';
-import heloLulu from '@/pages/home/file_000000005a987246a247f74797bca5e8 1 (1).webp';
+import heloLulu from '@/pages/home/file_000000005a987246a247f74797bca5e8 1 (2).png';
 
 const Hero = () => {
   return (
     <Section>
       <h1>AiCook </h1>
-      <img src={heloLulu} alt="" />
+      <img src={heloLulu} width="240px" height="240px" />
     </Section>
   );
 };
