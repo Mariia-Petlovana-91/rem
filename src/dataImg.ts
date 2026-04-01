8d224af753f3de0972ed6b00e345d77378a1a6e8
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2026-04-01 23:25:28 +0200

    add data images and write translation

---
+++ src/dataImg.ts
@@ -0,0 +1,57 @@
+export const images = [
+  {
+    mobile: '/images/catbox.png',
+    desktop: '/images/catboxmob.png',
+    textKey: 'img.catbox',
+  },
+  {
+    mobile: '/images/catfish.png',
+    desktop: '/images/catfishmob.png',
+    textKey: 'img.catfish',
+  },
+  {
+    mobile: '/images/catkoc.png',
+    desktop: '/images/catkocmob.png',
+    textKey: 'img.catkoc',
+  },
+  {
+    mobile: '/images/catkoc2.png',
+    desktop: '/images/catkoc2mob.png',
+    textKey: 'img.catkoc2',
+  },
+  {
+    mobile: '/images/catlove.png',
+    desktop: '/images/catboxlove.png',
+    textKey: 'img.catlove',
+  },
+  {
+    mobile: '/images/catlustro.png',
+    desktop: '/images/catlustromob.png',
+    textKey: 'img.catlustro',
+  },
+  {
+    mobile: '/images/catmetelek.png',
+    desktop: '/images/catmetelekmob.png',
+    textKey: 'img.catmetelek',
+  },
+  {
+    mobile: '/images/catpapirowatorba.png',
+    desktop: '/images/catpapirowatorbamob.png',
+    textKey: 'img.catpapirowatorba',
+  },
+  {
+    mobile: '/images/catplay.png',
+    desktop: '/images/catplaymob.png',
+    textKey: 'img.catplay',
+  },
+  {
+    mobile: '/images/catplay2.png',
+    desktop: '/images/catplay2mob.png',
+    textKey: 'img.catplay2',
+  },
+  {
+    mobile: '/images/catsit.png',
+    desktop: '/images/catsitmob.png',
+    textKey: 'img.catsit',
+  },
+];
